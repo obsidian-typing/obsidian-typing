b01a3726522a4100b7209b643a54a4856b3e000e
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-04-25 14:34:14 +0000

    feat: Add validation for Choice and Tag fields

--- src/typing/field_type/choice.tsx
+++ src/typing/field_type/choice.tsx
@@ -2,6 +2,7 @@ import { Visitors } from "src/language";
 import { Pickers } from "src/ui";
 import { field, stripQuotes } from "src/utilities";
 import { FieldType } from "./base";
+import { report, Validation } from "src/validation";
 
 export class Choice extends FieldType<Choice> {
     name = "Choice";
@@ -11,6 +12,24 @@ export class Choice extends FieldType<Choice> {
 
     @field()
     public fuzzy: boolean = true;
+
+    validate(target: Validation.Target<unknown>): void | Promise<void> {
+        if (typeof target.value === "string") {
+            return this.validateTyped(target.asTyped(target.value));
+        } else {
+            report(target, {
+                message: `Field ${target.path} must be a string`
+            });
+        }
+    }
+
+    validateTyped(target: Validation.Target<string>): void | Promise<void> {
+        if (!this.options.includes(target.value)) {
+            report(target, {
+                message: `Field ${target.path} must be one of the following values: ${this.options.join(", ")}`
+            });
+        }
+    }
 
     Display: FieldType["Display"] = ({ value }) => {
         if (value && this.context?.inList) {
