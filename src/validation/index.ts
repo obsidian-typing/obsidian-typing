c95d0c8e47a2888aeb5bb2331557124a213b48b9
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-04-25 14:32:59 +0000

    Add Validator/TypedValidator interfaces

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -7,6 +7,15 @@ export { ValidationResult } from "./validation_result";
 export type ValidationMessage = Validation.Message;
 export type ValidationTarget<T> = Validation.Target<T>;
 
+export interface Validator<T> {
+    validate(target: Validation.Target<T>): void | Promise<void>;
+}
+
+export interface TypedValidator<T> {
+    validate(target: ValidationTarget<unknown>): void | Promise<void>;
+    validateTyped(target: ValidationTarget<T>): void | Promise<void>;
+}
+
 export function report<T>(target: ValidationTarget<T>, params: Validation.MessageSpec): void {
     target.report(params);
 }
