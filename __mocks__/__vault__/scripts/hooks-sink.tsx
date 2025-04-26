5f1dc6c2ea6b49b3bf2cb97524fa889a5c1683f4
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-04-26 11:37:35 +0000

    fix: Work around the fact that TS transpilation is not yet supported

--- __mocks__/__vault__/scripts/hooks-sink.tsx
+++ __mocks__/__vault__/scripts/hooks-sink.tsx
@@ -1,5 +1,7 @@
-export const hookCalls: { hookName: string, ctx: any }[] = [];
+// @ts-ignore // Transpilation of TS files is not yet supported
+export const hookCalls /*: { hookName: string, ctx: any }[]*/ = [];
 
-export function appendHookCall(hookName: string, ctx: any) {
+// @ts-ignore // Transpilation of TS files is not yet supported
+export function appendHookCall(hookName /*: string*/, ctx /*: any*/) {
     hookCalls.push({ hookName, ctx });
 }
