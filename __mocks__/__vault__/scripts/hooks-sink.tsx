e6573052a1fda297aa6698213c0dd292530c1fda
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-04-26 19:54:06 +0000

    chore: Use TypeScript syntax in hook script fixtures

--- __mocks__/__vault__/scripts/hooks-sink.tsx
+++ __mocks__/__vault__/scripts/hooks-sink.tsx
@@ -1,7 +1,5 @@
-// @ts-ignore // Transpilation of TS files is not yet supported
-export const hookCalls /*: { hookName: string, ctx: any }[]*/ = [];
+export const hookCalls: { hookName: string, ctx: any }[] = [];
 
-// @ts-ignore // Transpilation of TS files is not yet supported
-export function appendHookCall(hookName /*: string*/, ctx /*: any*/) {
+export function appendHookCall(hookName: string, ctx: any) {
     hookCalls.push({ hookName, ctx });
 }
