3022974c378969964b57c3f3a9c947ff58f43bf4
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-04-27 12:20:47 +0200

    Revert "fix: Add proper handling of multi-platform modules to jest.config.js"
    This reverts commit 7b7074c9df1d64d30211f46e6eaf8737b62840f8.

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     moduleDirectories: ["node_modules", "src", "<rootDir>"],
-    haste: {
-        defaultPlatform: "node",
-    },
     transform: {
         "^.+\\.tsx?$": [
             "ts-jest",
