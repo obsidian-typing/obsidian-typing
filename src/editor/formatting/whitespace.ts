90b2cc9b8822f6bc264f11b395b82f3bda1f37c5
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-05-06 12:53:24 +0000

    feat: Implement whitespace manipulation in WhiteSpace.applySpacing

--- src/editor/formatting/whitespace.ts
+++ src/editor/formatting/whitespace.ts
@@ -3,11 +3,11 @@ import { SyntaxNode, SyntaxNodeRef } from "@lezer/common";
 import assert from "assert";
 import { Spacing } from "./spacing";
 
-function assertSameNode(a: SyntaxNodeRef | null, b: SyntaxNodeRef | null, message: string) {
-    assert(a?.type === b?.type, message);
-    assert(a?.to === b?.to, message);
-    assert(a?.from === b?.from, message);
-}
+// TODO: Review if we need to include more whitespace/linebreak characters in the regular expressions
+// TODO: Make these configurable for different languages?
+const LINE_FEED = /[\n\r]/u;
+const WHITE_SPACE = /[\s]/u;
+const ALL_WHITE_SPACE = /^[\s]*$/ug;
 
 /**
  * Represents and allows modifications to the whitespace before, after and between nodes.
@@ -30,9 +30,41 @@ export class WhiteSpace {
         return this.doc.slice(this.from, this.to);
     }
 
-    applySpacing(spacing: Spacing): ChangeSpec | null {
-        return null;
+    containsLineFeeds(): boolean {
+        return !!this.getString().match(LINE_FEED);
     }
+
+    containsOnlySpaces(): boolean {
+        return !!this.getString().match(ALL_WHITE_SPACE);
+    }
+
+    applySpacing(spacing: Spacing): ChangeSpec | null {
+        if (this.containsLineFeeds()) {
+            // TODO: Not yet implemented: Also format blank lines and line breaks, remove trailing spaces etc.
+            return null;
+        }
+
+        let match = this.getString().match(ALL_WHITE_SPACE);
+        if (!match) {
+            // Does contain other characters than just whitespaces.
+            // This is not expected to happen when languages are written in such a way
+            // that only spaces are not contained in any syntax node.
+            return null;
+        }
+
+        // TODO: Determine number of current spaces (the current implementation is very crude and does not work well e.g. for tabs)
+        let currentSpaces = match?.[0].length ?? 0;
+        let newSpaces = Math.clamp(currentSpaces, spacing.minSpaces, spacing.maxSpaces);
+        if (newSpaces === currentSpaces) {
+            // No change necessary
+            // TODO: Normalization of unusual space characters?
+            return null;
+
+        }
+
+        // Update the number of spaces
+        return { from: this.from, to: this.to, insert: " ".repeat(newSpaces) };
+    };
 
     /**
      * Gets the whitespace between the specified two nodes.
@@ -79,4 +111,10 @@ export class WhiteSpace {
         let to = before?.from ?? parent?.to ?? doc.length;
         return new WhiteSpace(doc, from, to, after ?? null, before ?? null);
     }
+}
+
+function assertSameNode(a: SyntaxNodeRef | null, b: SyntaxNodeRef | null, message: string) {
+    assert(a?.type === b?.type, message);
+    assert(a?.to === b?.to, message);
+    assert(a?.from === b?.from, message);
 }
