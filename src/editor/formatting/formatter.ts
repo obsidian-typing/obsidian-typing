67c8d40b53d3f8fadb7718cccf30e124c4d913b5
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-05-06 09:46:33 +0000

    fix: Traverse the whole tree in the formatter instead of only a single tree level

--- src/editor/formatting/formatter.ts
+++ src/editor/formatting/formatter.ts
@@ -36,20 +36,50 @@ export function formatTree(state: EditorState, node: SyntaxNode, rules: SpacingR
 
     let doc: Text = state.doc;
     let cursor = node.cursor(IterMode.IgnoreMounts | IterMode.IgnoreOverlays);
+    let top = cursor.node;
+
+    // TODO: Optimization: This currently reifies the whole tree into SyntaxNode
+    //       Because it needs to keep track of more than one node at a time.
+    //       This could be optimized to a certain degree by only storing the minimal
+    //       information needed (i.e. type, from, to) for non-current nodes.
 
     let parent = cursor.node;
-    let parentRange = { from: parent.from, to: parent.to };
     let prev: SyntaxNode | null = null;
-    if (cursor.firstChild()) {
-        do {
-            // Handle whitespace at start of parent and between children
+    while (true) {
+        let startingPoint = cursor.node;
+        if (cursor.firstChild()) {
+            // Handle whitespace at start of parent
+            parent = startingPoint;
+            prev = null;
+            updateSpacing(parent, null, cursor.node);
+            prev = cursor.node;
+        } else if (cursor.nextSibling()) {
+            // Handle whitespace between nodes
             updateSpacing(parent, prev, cursor.node);
             prev = cursor.node;
-        } while (cursor.nextSibling());
+        } else {
+            if (prev) {
+                // Handle whitespace at end of parent
+                updateSpacing(parent, prev, cursor.node);
+                prev = null;
+            }
 
-        // Handle whitespace at end of parent
-        updateSpacing(parent, prev, cursor.node);
-        prev = null;
+            // Continute iteration at parent
+            if (!cursor.next() || cursor.node === top) {
+                // We have reached the top of our (sub-)tree
+                break;
+            }
+
+            // TODO: We should always be inside a subtree and thus parent should never be null here
+            parent = cursor.node.parent!;
+            // TODO: Optimization: Find a way to get rid of bidirectional tree traversal.
+            //       This probably involves checking the rule conditions in lock-step with traversing the tree.
+            prev = cursor.node.prevSibling;
+
+            // Handle whitespace between nodes
+            updateSpacing(parent, prev, cursor.node);
+            prev = cursor.node;
+        }
     }
 
     return { changes };
